f4297f8c345f1b8c73a2066f1d5db2f19d21a61c
Fazil M <dumpdusty@gmail.com>
2024-05-15 11:32:49 -0700

    clean-up

--- test/example.spec.js
+++ test/example.spec.js
@@ -5,8 +5,3 @@ describe('VERIFY SIMPLE MATH FUNCTIONS', () => {
     expect(4 + 4).eq(8);
   });
 });
-
-const object = { a: 1, b: 'a', c: 3 };
-const printHell = () => {
-  console.log('HellYA!!');
-};
