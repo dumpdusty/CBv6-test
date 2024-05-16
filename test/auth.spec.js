27cfd59b55d1d8faa4873f74ea82c1d69dbeb84b
Fazil M <dumpdusty@gmail.com>
2024-05-15 18:39:32 -0700

    add dotenv

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,15 +1,18 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import 'dotenv/config'
 
 
 describe('AUTHENTICATION', () => {
     it('login with valid credentials', async() => {
-       const res = await request('https://clientbase-server.herokuapp.com/v6/')
+       const res = await request(process.env.BASE_URL)
         .post('user/login')
-        .send({email: 'jacksparrow@pirate.com', password: 'Pirate666!'})
+        .send({email: process.env.EMAIL, password: process.env.PASSWORD})
 
        expect(res.status).to.eq(200)
 
        expect(res.body.message).to.equal('Auth success')
+
+       console.log(res.body);
     });
 });
