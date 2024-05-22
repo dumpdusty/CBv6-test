1e0fffa4f19ce7ff56aab6d48d855b6aaa7ebdb2
Fazil M <81851166+dumpdusty@users.noreply.github.com>
2024-05-22 13:09:10 -0700

    add auth negative tests (#5)

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,15 +1,55 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import 'dotenv/config'
 
 
 describe('AUTHENTICATION', () => {
     it('login with valid credentials', async() => {
-       const res = await request('https://clientbase-server.herokuapp.com/v6/')
+       const res = await request(process.env.BASE_URL)
         .post('user/login')
-        .send({email: 'jacksparrow@pirate.com', password: 'Pirate666!'})
+        .send({email: process.env.EMAIL, password: process.env.PASSWORD})
 
        expect(res.status).to.eq(200)
-
-       expect(res.body.message).to.equal('Auth success')
     });
+
+    it('login with valid credentials', async() => {
+        const res = await request(process.env.BASE_URL)
+         .post('user/login')
+         .send({email: process.env.EMAIL, password: process.env.PASSWORD})
+ 
+        expect(res.body.message).to.equal('Auth success')
+     });
+
+    it('login with invalid email', async() => {
+        const res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({email: process.env.INVALID, password: process.env.PASSWORD})
+
+       expect(res.status).to.eq(400)
+    });
+
+    it('login with invalid email', async() => {
+        const res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({email: process.env.INVALID, password: process.env.PASSWORD})
+
+       expect(res.body.message).to.eq('Auth failed')
+    });
+
+    it('login with invalid password', async() => {
+        const res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({email: process.env.EMAIL, password: process.env.INVALID})
+
+       expect(res.status).to.eq(400)
+    });
+    
+    it('login with invalid password', async() => {
+        const res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({email: process.env.EMAIL, password: process.env.INVALID})
+
+       expect(res.body.message).to.eq('Auth failed')
+    });
+
 });
