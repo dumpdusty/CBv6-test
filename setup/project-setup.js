7589aae661e167f187ccd53c948c5ed0d4a1166e
Fazil M <81851166+dumpdusty@users.noreply.github.com>
2024-05-22 18:07:56 -0700

    add hooks (#6)

---
+++ setup/project-setup.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import request from'supertest';
+
+before(async() => {
+    const response = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD });
+
+        process.env.TOKEN = response.body.payload.token
+});
+
--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,55 +1,53 @@
 import request from 'supertest';
 import { expect } from 'chai';
-import 'dotenv/config'
-
 
 describe('AUTHENTICATION', () => {
-    it('login with valid credentials', async() => {
-       const res = await request(process.env.BASE_URL)
+  describe.only('POSITIVE', () => {
+    let res;
+    before(async () => {
+      res = await request(process.env.BASE_URL)
         .post('user/login')
-        .send({email: process.env.EMAIL, password: process.env.PASSWORD})
-
-       expect(res.status).to.eq(200)
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD });
     });
 
-    it('login with valid credentials', async() => {
-        const res = await request(process.env.BASE_URL)
-         .post('user/login')
-         .send({email: process.env.EMAIL, password: process.env.PASSWORD})
- 
-        expect(res.body.message).to.equal('Auth success')
-     });
-
-    it('login with invalid email', async() => {
-        const res = await request(process.env.BASE_URL)
-        .post('user/login')
-        .send({email: process.env.INVALID, password: process.env.PASSWORD})
-
-       expect(res.status).to.eq(400)
+    it('verify response status code', async () => {
+      expect(res.status).to.eq(200);
     });
 
-    it('login with invalid email', async() => {
-        const res = await request(process.env.BASE_URL)
-        .post('user/login')
-        .send({email: process.env.INVALID, password: process.env.PASSWORD})
+    it('verify response body message', async () => {
+      expect(res.body.message).to.equal('Auth success');
+    });
+  });
 
-       expect(res.body.message).to.eq('Auth failed')
+  describe('NEGATIVE', () => {
+    let res;
+    before(async() => {
+        res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({ email: process.env.INVALID, password: process.env.PASSWORD });
+    });
+    it('verify response status code', async () => {
+      expect(res.status).to.eq(400);
     });
 
-    it('login with invalid password', async() => {
-        const res = await request(process.env.BASE_URL)
-        .post('user/login')
-        .send({email: process.env.EMAIL, password: process.env.INVALID})
-
-       expect(res.status).to.eq(400)
-    });
-    
-    it('login with invalid password', async() => {
-        const res = await request(process.env.BASE_URL)
-        .post('user/login')
-        .send({email: process.env.EMAIL, password: process.env.INVALID})
-
-       expect(res.body.message).to.eq('Auth failed')
+    it('verify response body message', async () => {
+      expect(res.body.message).to.eq('Auth failed');
     });
 
+    it('verify response status code', async () => {
+      const res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({ email: process.env.EMAIL, password: process.env.INVALID });
+
+      expect(res.status).to.eq(400);
+    });
+
+    it('verify response body message', async () => {
+      const res = await request(process.env.BASE_URL)
+        .post('user/login')
+        .send({ email: process.env.EMAIL, password: process.env.INVALID });
+
+      expect(res.body.message).to.eq('Auth failed');
+    });
+  });
 });
