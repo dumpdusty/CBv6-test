a5aeb29d8cdd45cdb531d31b26730dd0ef937576
Fazil M <dumpdusty@gmail.com>
2024-05-22 18:54:21 -0700

    generalHelper

---
+++ helpers/generalHelper.js
@@ -0,0 +1,11 @@
+import request from 'supertest'
+
+export function login(email, password){
+    return request(process.env.BASE_URL)
+    .post('user/login')
+    .send({ email: email, password: password });
+}
+
+
+
+
