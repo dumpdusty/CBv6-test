12da9f6388d1312a2242e8e7dc0a118aafe648a1
Fazil M <dumpdusty@gmail.com>
2024-05-29 17:40:22 -0700

    exclude setup file

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,5 @@ module.exports = {
     require: ['@babel/register'],
     timeout: '20000',
     exclude: ['test/example.spec.js'],
-    file: 'setup/project-setup.js'
+    // file: 'setup/project-setup.js'
   };
