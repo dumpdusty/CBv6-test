97a8511be211fe4c21ec5e974e3bfd4963010b06
Fazil M <dumpdusty@gmail.com>
2024-06-05 20:51:09 -0700

    exclude ignup tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     require: ['@babel/register'],
     timeout: '20000',
-    exclude: ['test/example.spec.js', 'test/register.spec.js'],
+    exclude: ['test/example.spec.js', 'test/signup.spec.js'],
     file: 'setup/project-setup.js'
   };
