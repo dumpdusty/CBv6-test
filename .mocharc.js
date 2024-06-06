af3c9aeab5fe7df1ae63e9fe665c4909a42a3895
Fazil M <dumpdusty@gmail.com>
2024-06-05 20:48:51 -0700

    exclude register tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     require: ['@babel/register'],
     timeout: '20000',
-    exclude: ['test/example.spec.js'],
+    exclude: ['test/example.spec.js', 'test/register.spec.js'],
     file: 'setup/project-setup.js'
   };
