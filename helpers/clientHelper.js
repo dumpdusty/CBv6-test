4ea72e94e249227777622563d3510c161d4b3cfa
Fazil M <dumpdusty@gmail.com>
2024-06-26 19:00:09 -0700

    add more clients test

--- helpers/clientHelper.js
+++ helpers/clientHelper.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 const chance = require('chance').Chance();
 
-export function createClient(name, phone, email='', description=''){
+export function create(name, phone, email='', description=''){
     return request(process.env.BASE_URL)
         .post('client')
         .set('Authorization', process.env.TOKEN)
@@ -11,4 +11,17 @@ export function createClient(name, phone, email='', description=''){
           email,
           description,
         });
+}
+
+export function getAll(){
+    return request(process.env.BASE_URL)
+    .post('client/search')
+    .set('Authorization', process.env.TOKEN)
+    .send({limit: 20})
+}
+
+export function deleteSingle(id){
+    return request(process.env.BASE_URL)
+    .delete('client/' + id)
+    .set('Authorization', process.env.TOKEN)
 }
