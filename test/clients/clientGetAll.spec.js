7e1fb9c6b1a84bb531c4012b28196dde25bf0c17
Fazil M <dumpdusty@gmail.com>
2024-07-01 16:23:14 -0700

    add more assertions

--- test/clients/clientGetAll.spec.js
+++ test/clients/clientGetAll.spec.js
@@ -7,11 +7,24 @@ describe('GET ALL CLIENTS', () => {
     let res;
     describe('get all clients POSITIVE', () => {
         before(async() => {
+            await Client.create()
             res = await Client.getAll()
         });
 
-        it('verify status code', () => {
-            expect(res.statusCode).to.eq(200)
-        });
+        it('verify response status', () => {
+            expect(res.statusCode).eq(200);
+          });
+      
+          it('verify response message', () => {
+            expect(res.body.message).eq('ClientSearch ok');
+          });
+      
+          it('verify response contains array', () => {
+            expect(res.body.payload.items).to.be.a('array');
+          });
+      
+          it('verify response array has at least one element', () => {
+            expect(res.body.payload.items.length).to.be.above(0);
+          });
     });
 });
