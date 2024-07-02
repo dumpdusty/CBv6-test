226c52d3bb0db17769b64ee9c49f146766708382
Fazil M <81851166+dumpdusty@users.noreply.github.com>
2024-07-02 16:37:29 -0700

    This PR adds `clientsGetAll` negative tests (#26)
    * renaming & negative clientsGetAll
    
    * fix typos

--- helpers/clientHelper.js
+++ helpers/clientHelper.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 const chance = require('chance').Chance();
 
-export function create(name=chance.name(), phone=chance.phone(), email='', description=''){
+export function createClient(name=chance.name(), phone=chance.phone(), email='', description=''){
     return request(process.env.BASE_URL)
         .post('client')
         .set('Authorization', process.env.TOKEN)
@@ -13,14 +13,14 @@ export function create(name=chance.name(), phone=chance.phone(), email='', descr
         });
 }
 
-export function getAll(){
+export function getAllClients(token = process.env.TOKEN){
     return request(process.env.BASE_URL)
     .post('client/search')
-    .set('Authorization', process.env.TOKEN)
-    .send({limit: 20})
+    .set('Authorization', token)
+    .send({limit: 50})
 }
 
-export function deleteSingle(id){
+export function deleteClient(id){
     return request(process.env.BASE_URL)
     .delete('client/' + id)
     .set('Authorization', process.env.TOKEN)
--- test/clients/clientGetAll.spec.js
+++ test/clients/clientGetAll.spec.js
@@ -1,14 +1,14 @@
 import request from 'supertest';
 const chance = require('chance').Chance();
 import { expect } from 'chai';
-import * as Client from '../../helpers/clientHelper';
+import * as clientHelper from '../../helpers/clientHelper';
 
 describe('GET ALL CLIENTS', () => {
     let res;
     describe('get all clients POSITIVE', () => {
         before(async() => {
-            await Client.create()
-            res = await Client.getAll()
+            await clientHelper.createClient()
+            res = await clientHelper.getAllClients()
         });
 
         it('verify response status', () => {
@@ -26,5 +26,22 @@ describe('GET ALL CLIENTS', () => {
           it('verify response array has at least one element', () => {
             expect(res.body.payload.items.length).to.be.above(0);
           });
+    });
+
+    describe('get all clients NEGATIVE', () => {
+      describe('get clients without token', () => {
+        before(async () => {
+          await clientHelper.createClient();
+          res = await clientHelper.getAllClients('');
+        });
+  
+        it('verify response status', () => {
+          expect(res.statusCode).eq(400);
+        });
+  
+        it('verify response message', () => {
+          expect(res.body.message).eq('Auth failed');
+        });
+      });
     });
 });
