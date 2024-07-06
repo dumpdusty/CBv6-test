5b820ddbc4bbf4547caa398338d2f5155b254a1a
Rafail Ferre <qa.rafailferre@gmail.com>
2024-07-06 17:09:42 -0400

    add negative tests vendor delete

--- test/vendors/vendorDelete.spec.js
+++ test/vendors/vendorDelete.spec.js
@@ -4,7 +4,7 @@ import * as vendorHelper from '../../helpers/vendorHelper';
 
 describe('DELETE VENDOR', () => {
     let res;
-    describe('delete vendor POSITIVE', () => {
+    describe('POSITIVE: DELETE VENDOR BY ID', () => {
         before(async() => {
 
         const vendorId = (await vendorHelper.createVendor(vendorHelper.vendorData)).body.payload
@@ -17,6 +17,38 @@ describe('DELETE VENDOR', () => {
         });
         it('verify response message', async () => {
             expect(res.body.message).to.eq('Vendor deleted');
+        })
+    });
+    describe('NEGATIVE: DELETE VENDOR WITH INVALID ID', () => {
+        before(async() => {
+
+        const inavalidVendorId = (await vendorHelper.createVendor(vendorHelper.vendorData)).body.payload + 'invalid'
+        
+        res = await vendorHelper.deleteVendor(inavalidVendorId)
+        });
+
+        it('verify status code', async () => {
+            expect(res.status).to.eq(400);    
+        });
+
+        it('verify response message', async () => {
+            expect(res.body.message).to.eq('Vendor delete error');
+        })
+    });
+    describe('NEGATIVE: DELETE VENDOR WITHOUT ID', () => {
+        before(async() => {
+
+        const VendorId = (await vendorHelper.createVendor(vendorHelper.vendorData)).body.payload
+        
+        res = await vendorHelper.deleteVendor()
+        });
+
+        it('verify status code', async () => {
+            expect(res.status).to.eq(400);
+        });
+
+        it('verify response message', async () => {
+            expect(res.body.message).to.eq('Vendor delete error');
         })
     });
 });
