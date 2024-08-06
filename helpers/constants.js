cb2cc4bb89958cd7444d70cc85aa86658b518745
Rafail Ferre <154566757+RafailFerre@users.noreply.github.com>
2024-08-06 19:55:10 -0400

    Get company profile (#39)
    * create companyGet.spec, companyHelper, add userId in constants, userHelper for alternative getting userId
    
    * new update get-company-profile

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,6 +1,8 @@
 import * as serviceHelper from './serviceHelper';
 import * as vendorHelper from './vendorHelper';
+import * as user from './userHelper';
 const chance = require('chance').Chance();
+
 
 export const newEmail = () => {
   return 'user_' + Date.now() + '@pirate.com';
@@ -20,3 +22,11 @@ export const vendorId = async () => {
 export const serviceId = async () => {
   return (await serviceHelper.createService(serviceHelper.serviceData(await vendorId()))).body.payload;
 };
+
+export const userId = async () => {
+  return (await user.login(user.userData.email, user.userData.password)).body.payload.userId; 
+}
+
+export const companyId = async () => {
+  return (await user.getUser(await user.userId())).body.payload.companyAccount._id; 
+}
--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -6,6 +6,7 @@ export const newEmail = () => {
   };
   
   export const userData = {
+    companyName: chance.company(),
     firstName: chance.first(),
     lastName: chance.last(),
     email: newEmail(),
@@ -20,6 +21,9 @@ export function login(email, password) {
     return request(process.env.BASE_URL)
       .post('user/login')
       .send({ email, password });
+  }
+  export const userId = async () => {
+    return (await login(userData.email, userData.password)).body.payload.userId; 
   }
 
   export function getUser(userId){
