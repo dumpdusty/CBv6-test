257d9a5375e16aa2ae3945f9ab55881ec70e9bfb
DashaGKH <keshonti2016@gmail.com>
2024-08-11 16:24:21 -0700

    fixed serviceUpdate - added negative test update w/o auth

--- test/services/serviceUpdate.spec.js
+++ test/services/serviceUpdate.spec.js
@@ -3,7 +3,7 @@ import * as serviceHelper from '../../helpers/serviceHelper';
 import * as vendorHelper from '../../helpers/vendorHelper';
 
 describe('SERVICE UPDATE', () => {
-  let service, vendorId, updateService, updatedData, getService;
+  let service, vendorId, updateService, updatedData, getService, getUpdService;
   before(async () => {
     vendorId = (await vendorHelper.createVendor(vendorHelper.vendorData)).body.payload;
     updatedData = { ...serviceHelper.serviceData(vendorId), name: 'Updated name', clientPrice: 0, vendorPrice: 0 };
@@ -18,6 +18,8 @@ describe('SERVICE UPDATE', () => {
     before(async () => {
       getService = await serviceHelper.getServiceById(service.body.payload, process.env.TOKEN);
       updateService = await serviceHelper.updateServiceById(service.body.payload, updatedData, process.env.TOKEN);
+      getUpdService = await serviceHelper.getServiceById(service.body.payload, process.env.TOKEN);
+      console.log(getUpdService.body.payload.name)
     });
 
     it('verify status code for update request is 200', () => {
@@ -28,19 +30,31 @@ describe('SERVICE UPDATE', () => {
       expect(updateService.body.message).to.eq('Service updated');
     });
 
+    it('verify updated service name ', () => {
+      expect(getUpdService.body.payload.name).to.eq('Updated name');
+    });
+
     it('verify updated service success is true', () => {
       expect(updateService.body.success).to.eq(true);
     });
 
-    it('verify updated service name ', () => {
+    it('verify old service name not eq updated one', () => {
       expect(getService.body.payload.name).to.not.eq('Updated name');
     });
 
     it('verify clientPrice for updated service ', () => {
+      expect(getUpdService.body.payload.clientPrice).to.eq(0);
+    });
+
+    it('verify old clientPrice is not eq updated clientPrice ', () => {
       expect(getService.body.payload.clientPrice).to.not.eq(0);
     });
 
     it('verify vendorPrice for updated service ', () => {
+      expect(getUpdService.body.payload.vendorPrice).to.eq(0);
+    });
+
+    it('verify old vendorPrice is not eq updated vendorPrice ', () => {
       expect(getService.body.payload.vendorPrice).to.not.eq(0);
     });
   });
